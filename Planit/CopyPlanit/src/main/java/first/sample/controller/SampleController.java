866796cf274cf17bd7dcb2cc9279a1e6c0b2a583
clachic00 <anajrwk2@gmail.com>
2020-12-04 02:30:12 +0900

    .

--- Planit/CopyPlanit/src/main/java/first/sample/controller/SampleController.java
+++ Planit/CopyPlanit/src/main/java/first/sample/controller/SampleController.java
@@ -2,19 +2,25 @@ package first.sample.controller;
 
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.sample.service.SampleService;
+
 @Controller
 public class SampleController {
 	protected Log log = LogFactory.getLog(SampleController.class);
 
+	@Resource(name="sampleService")
+	private SampleService sampleService;
+
 	@RequestMapping("/home")
 	public ModelAndView openSampleList(Map<String, Object> commandMap) throws Exception {
-		System.out.println("################################");
 		log.info("DdDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
 		ModelAndView mv = new ModelAndView("");
 		log.debug("인터셉터 테스트");
